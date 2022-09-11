fcd91d099a536422c7e882d6c11a020e58e2e88b
pdcolandrea <colandreapaul@gmail.com>
2022-09-11 19:34:59 -0400

    feat: mobile friendly navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,40 +1,52 @@
 import { asset } from "https://deno.land/x/fresh@1.1.0/runtime.ts";
-import { LocationPin } from "./Icons/HeroIcons.tsx";
+import { BarsIcon, LocationPin } from "./Icons/HeroIcons.tsx";
 
 export default function NavBar() {
   return (
-    <div class="flex flex-row justify-between items-center shadow-md mb-8 py-6 px-20">
-      <div class="flex flex-row items-center">
+    <>
+      <div class="flex flex-row justify-between items-center shadow-md mb-8 py-6 px-20 md:flex sm:hidden">
+        <div class="flex flex-row items-center">
+          <img
+            src={asset("./bux_logo.png")}
+            href="#"
+            class="h-16 w-16 mr-6 cursor-pointer"
+          />
+          <ul class="pr-5 font-bold text-sbbrown hover:text-sbgreenalt cursor-pointer">
+            MENU
+          </ul>
+          <ul class="pr-5 font-bold text-sbbrown hover:text-sbgreenalt cursor-pointer">
+            REWARDS
+          </ul>
+          <ul class="pr-5 font-bold text-sbbrown hover:text-sbgreenalt cursor-pointer">
+            GIFT CARDS
+          </ul>
+        </div>
+
+        <div class="flex flex-row items-center">
+          <span className="flex font-semibold group hover:text-sbgreenalt cursor-pointer ">
+            <LocationPin />
+            Find a store
+          </span>
+
+          <button class="border-1 border-black px-3 py-1 rounded-2xl ml-6 font-semibold hover:bg-greyish">
+            Sign in
+          </button>
+
+          <button class="bg-black text-white px-3 py-1 rounded-2xl ml-4 font-semibold hover:bg-warmblack">
+            Join Now
+          </button>
+        </div>
+      </div>
+
+      <div class="flex flex-row justify-between items-center shadow-md mb-8 py-6 px-10 md:hidden sm:flex">
         <img
           src={asset("./bux_logo.png")}
           href="#"
           class="h-16 w-16 mr-6 cursor-pointer"
         />
-        <ul class="pr-5 font-bold text-sbbrown hover:text-sbgreenalt cursor-pointer">
-          MENU
-        </ul>
-        <ul class="pr-5 font-bold text-sbbrown hover:text-sbgreenalt cursor-pointer">
-          REWARDS
-        </ul>
-        <ul class="pr-5 font-bold text-sbbrown hover:text-sbgreenalt cursor-pointer">
-          GIFT CARDS
-        </ul>
+
+        <BarsIcon />
       </div>
-
-      <div class="flex flex-row items-center">
-        <span className="flex font-semibold group hover:text-sbgreenalt cursor-pointer ">
-          <LocationPin />
-          Find a store
-        </span>
-
-        <button class="border-1 border-black px-3 py-1 rounded-2xl ml-6 font-semibold hover:bg-greyish">
-          Sign in
-        </button>
-
-        <button class="bg-black text-white px-3 py-1 rounded-2xl ml-4 font-semibold hover:bg-warmblack">
-          Join Now
-        </button>
-      </div>
-    </div>
+    </>
   );
 }
